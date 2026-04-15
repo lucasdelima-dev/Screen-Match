ac3c3fa287d1831015a8305b89e2653f8c15a95d
Lucas de Lima <lucas.delima2545@gmail.com>
2026-04-15 10:00:20 -0300

    Trabalhando com Herança e Parents
    Signed-off-by: Lucas de Lima <lucas.delima2545@gmail.com>

--- src/Modelo/Serie.php
+++ src/Modelo/Serie.php
@@ -1,12 +1,14 @@
 <?php
 
-class Filme extends Titulo
+class Serie extends Titulo
 {
     public function __construct(
         string $nome,
         int $anoLancamento,
         Genero $genero,
-        public readonly int $duracaoEmMinutos,
+        public readonly int $temporadas,
+        public readonly int $episodiosPorTemporadas,
+        public readonly int $minutosPorEpidio
     ) {
         parent::__construct($nome, $anoLancamento, $genero);
     }
